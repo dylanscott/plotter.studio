80133a063209095757db652e5a7e25aa40bcad62
Dylan Scott <dylan.scott@gmail.com>
2026-01-09 17:46:56 -0800

    wasm-bindgen

--- plotter-wasm/src/lib.rs
+++ plotter-wasm/src/lib.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {name}!"));
 }
