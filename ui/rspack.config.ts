f960dd8b234c77162e96e8c3a75b0464d199fa3e
Dylan Scott <dylan.scott@gmail.com>
2026-01-09 18:00:38 -0800

    rspack

---
+++ ui/rspack.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "@rspack/cli";
+import { rspack } from "@rspack/core";
+
+export default defineConfig({
+  entry: {
+    main: "./src/index.ts",
+  },
+  plugins: [
+    new rspack.HtmlRspackPlugin({
+      title: "plotter.studio",
+    }),
+  ],
+});
