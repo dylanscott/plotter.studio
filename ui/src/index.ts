3bcae9643cc845a300adaf56a654110265939926
Dylan Scott <dylan.scott@gmail.com>
2026-01-10 19:07:59 -0800

    format everything

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -1,7 +1,4 @@
 import { mount } from "svelte";
 import App from "./App.svelte";
 
-// @ts-expect-error
-const app = mount(App, {
-  target: document.body,
-});
+mount(App, { target: document.body });
