21a18e334e743fc3e5abbd104ecf6b2b7db9cfbe
Dylan Scott <dylan.scott@gmail.com>
2026-01-14 20:21:14 -0800

    expose digest_svg via wasm

--- plotter-wasm/src/lib.rs
+++ plotter-wasm/src/lib.rs
@@ -1,11 +1,13 @@
+use serde_wasm_bindgen::{from_value, to_value};
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::{JsError, JsValue};
+
+use plotter::simplify::{digest_svg, DigestOptions};
 
 #[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {name}!"));
+pub fn simplify(svg: &str, opts: JsValue) -> Result<JsValue, JsError> {
+    let digest_opts: DigestOptions = from_value(opts)?;
+    let digest_result =
+        digest_svg(svg, &digest_opts).map_err(|err| JsError::new(&err.to_string()))?;
+    Ok(to_value(&digest_result)?)
 }
